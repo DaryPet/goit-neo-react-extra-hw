b0747727665e33f84019a4744b56410e36916b42
DaryPet <d7akkord@gmail.com>
2024-05-14 19:02:26 +0200

    private

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -7,29 +7,55 @@ const authSlice = createSlice({
     user: {
       name: null,
       email: null,
+      loading: false,
+      error: false,
     },
     token: null,
     isLoggedIn: false,
   },
   extraReducers: (builder) =>
     builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      })
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
       })
       .addCase(login.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addDefaultCase(logout.fulfilled, (state) => {
+      .addCase(login.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      })
+      .addCase(logout.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = {
           name: null,
           email: null,
         };
         state.token = null;
         state.isLoggedIn = false;
+      })
+      .addCase(logout.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
       }),
 });
 
--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,6 +1,7 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { addContact, fetchContacts, deleteContact } from "./operations";
 import { selectNameFilter } from "../filters/slice";
+import { logout } from "../auth/operations";
 
 const contactSlice = createSlice({
   name: "contacts",
@@ -48,6 +49,9 @@ const contactSlice = createSlice({
       .addCase(deleteContact.rejected, (state) => {
         state.loading = false;
         state.error = true;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        (state.items = []), (state.loading = false), (state.error = null);
       }),
 });
 
