1c7c6babb55e58e3368191856a2887f63d50d0dd
DaryPet <d7akkord@gmail.com>
2024-05-14 14:29:37 +0200

    styles_reset

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,11 +2,12 @@
 import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UseMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
+import css from "./AppBar.module.css";
 
 export default function AppBar() {
   // const isLoggedIn = useSelector();
   return (
-    <header>
+    <header className={css.header}>
       <Navigation />
       {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
       <UserMenu />
