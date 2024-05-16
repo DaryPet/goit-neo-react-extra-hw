a4e997fac5c1b442dd92193899dcba5979a81fd9
DaryPet <d7akkord@gmail.com>
2024-05-16 14:48:59 +0200

    modal

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,14 +2,21 @@ import css from "./Contact.module.css";
 import { RiUser3Fill } from "react-icons/ri";
 import { BiSolidPhone } from "react-icons/bi";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { deleteContact } from "../../redux/contacts/operations";
 import { Delete } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import Modal from "react-modal";
 
 export default function Contacts({ data }) {
   const dispatch = useDispatch();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const handleDelete = () => dispatch(deleteContact(data.id));
+  // const handleDelete = () => dispatch(deleteContact(data.id));
+  const handleDelete = () => {
+    dispatch(deleteContact(data.id));
+    setModalIsOpen(false);
+  };
 
   return (
     <div className={css.container} key={data.id}>
@@ -28,10 +35,28 @@ export default function Contacts({ data }) {
         variant="contained"
         sx={{ backgroundColor: "#5757b4", marginTop: 2 }}
         startIcon={<Delete />}
-        onClick={handleDelete}
+        onClick={() => {
+          setModalIsOpen(true);
+        }}
       >
         Delete
       </Button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => {
+          setModalIsOpen(false);
+        }}
+      >
+        <h2>Are you sure you want to delete this contact?</h2>
+        <Button onClick={handleDelete}>Yes</Button>
+        <Button
+          onClick={() => {
+            setModalIsOpen(false);
+          }}
+        >
+          No
+        </Button>
+      </Modal>
     </div>
   );
 }
